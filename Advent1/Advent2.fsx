48e0536c1a426414277a4dfad87ff38e06a867fd
Anders <anders.s.birch1@gmail.com>
2017-12-03 17:52:07 +0100

    Added Advent 1.2 solution.

--- Advent1/Advent2.fsx
+++ Advent1/Advent2.fsx
@@ -21,20 +21,22 @@ let charToInt x =
     |'9' -> 9
     |'0' -> 0    
     |_ -> failwith "Not An Integer"
+
     
 // We assume that the input has size >= 2
-let Advent1 φfilePath =
+// We know that the lenght is even
+let Advent2 φfilePath =
     let input = (readLines φfilePath).ToCharArray()
     let len = input.Length
     let readRing x = readRingIndex input x len
-    let mutable last = charToInt input.[0]
-    let mutable current = charToInt input.[1]
+    let mutable halfway = charToInt input.[len/2]
+    let mutable current = charToInt input.[0]
     let mutable sum = 0
-    for i = 1 to len do
-        last <- charToInt (readRing (i - 1))
+    for i = 0 to len - 1 do
+        halfway <- charToInt (readRing (i + (len/2)))
         current <- charToInt (readRing i)
-        if last = current then
+        if halfway = current then
             sum <- sum + current
     sum
     
-printfn "The answer is %i" (Advent1 "Input1.txt");;
+printfn "The answer is %i" (Advent2 "Input1.txt");;
